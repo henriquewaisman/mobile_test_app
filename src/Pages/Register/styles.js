5f1621ca00ccf2e30045c8fc50df89fc5d279eac
henriquewaisman <100635335+henriquewaisman@users.noreply.github.com>
2023-06-16 10:53:49 -0300

    Create styles.js

---
+++ src/Pages/Register/styles.js
@@ -0,0 +1,36 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center'
+    },
+    userText: {
+        marginTop: '50%',
+        fontSize: '20px',
+    },
+    user: {
+        borderWidth: '1px',
+        borderRadius: '10%'
+    },
+    password: {
+        borderWidth: '1px',
+        borderRadius: '10%'
+    },
+    passwordText: {
+        fontSize: '20px',
+    },
+    email: {
+        borderWidth: '1px',
+        borderRadius: '10%'
+    },
+    emailText: {
+        fontSize: '20px',
+    },
+    link: {
+        marginTop: '10%',
+        borderWidth: '2px'
+    }
+
+})
+
+export default styles;
