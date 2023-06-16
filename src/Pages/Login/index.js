b9d09dbcab77bf5a55a9968f2f7ec07b23217bd7
henriquewaisman <100635335+henriquewaisman@users.noreply.github.com>
2023-06-16 10:52:35 -0300

    Update index.js

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,1 +1,17 @@
+import { View, Text, TextInput } from 'react-native';
+import { Link } from '@react-navigation/native';
+import styles from './styles';
 
+export default function Login(){
+    return(
+        <View style={styles.container}>
+            <Text style={styles.userText}>Usuário</Text>
+            <TextInput style={styles.user} placeholder="usuário"/>
+            <Text style={styles.passwordText}>Senha</Text>
+            <TextInput style={styles.password} placeholder="senha"/>
+            <Link to={{screen: 'Dashboard'}} style={styles.dashboardLink}>Login</Link>
+            <Link to={{screen: 'Register'}} style={styles.registerLink}>Não tem conta? Cadastre-se</Link>
+            <Link to={{screen: 'Password'}} style={styles.passwordLink}>Esqueci Minha Senha</Link>
+        </View>
+    )
+}
