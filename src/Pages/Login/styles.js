02aa6020e5f8b1017030fc5cee67885595cadd8f
henriquewaisman <100635335+henriquewaisman@users.noreply.github.com>
2023-06-16 10:52:44 -0300

    Update styles.js

--- src/Pages/Login/styles.js
+++ src/Pages/Login/styles.js
@@ -1,1 +1,37 @@
+import { StyleSheet } from "react-native";
 
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+    },
+    userText: {
+        marginTop: '50%',
+        fontSize: '20px'
+    },
+    passwordText: {
+        fontSize: '20px'
+    },
+    user:{
+        fontSize: '15px',
+        borderWidth: '1px',
+        borderRadius: '5%'
+        
+    },
+    password:{
+        fontSize: '15px',
+        borderWidth: '1px',
+        borderRadius: '5%'
+    },
+    dashboardLink:{
+        marginTop: '10%',
+        borderWidth: '2px',
+    },
+    registerLink: {
+        marginTop: '2%',
+    },
+    passwordLink: {
+        marginTop: '2%',
+    }
+})
+
+export default styles;
