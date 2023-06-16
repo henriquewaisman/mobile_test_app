1b1ab77683c689778b87f60b31ccbf2a0f23efdb
henriquewaisman <100635335+henriquewaisman@users.noreply.github.com>
2023-06-16 10:51:44 -0300

    Create styles.js

---
+++ src/Pages/Password/styles.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from "react-native";
+import Password from ".";
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center'
+    },
+    emailText: {
+        marginTop: '50%',
+        fontSize: '20px'
+    },
+    email: {
+        borderWidth: '1px',
+        borderRadius: '10%'
+    },
+    passwordText: {
+        fontSize: '20px',
+    },
+    password: {
+        borderWidth: '1px',
+        borderRadius: '10%'
+    },
+    link: {
+        marginTop: '10%',
+        borderWidth: '2px'
+    }
+
+})
+
+export default styles;
